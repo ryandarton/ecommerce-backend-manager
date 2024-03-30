9761be2399054461e988a1ade9f08ececb4dec47
Ryan Darton <ryan@timemachinevideo.com>
2024-03-29 22:48:18 -0600

    Update connectino.js to use .env variables.

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -6,6 +6,9 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PW,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
