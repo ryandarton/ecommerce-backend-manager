62ae7a9b21b277bce843bf20d67812292e73ab21
Ryan Darton <ryan@timemachinevideo.com>
2024-03-30 00:07:10 -0600

    Update category-routes per instructions in the challenge.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,117 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  // First, find the category by its `id` value to get the original data
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+
+      const originalData = dbCategoryData.get({ plain: true });
+
+      Category.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      })
+        .then((updateResponse) => {
+          Category.findOne({
+            where: {
+              id: req.params.id,
+            },
+          }).then((updatedDbCategoryData) => {
+            const customMessage = 'Category successfully updated.';
+
+            res.json({
+              original: originalData,
+              message: customMessage,
+              updated: updatedDbCategoryData.get({ plain: true }),
+            });
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-});
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      Category.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
 
+      res.json({
+        message: 'Category successfully deleted.',
+        original: dbCategoryData,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+//
 module.exports = router;
