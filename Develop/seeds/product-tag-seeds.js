831a8feada13f682bd0350c42311d83878344658
Ryan Darton <ryan@timemachinevideo.com>
2024-03-29 23:06:28 -0600

    Update the product-tag-seeds per the challenge instructions.

--- Develop/seeds/product-tag-seeds.js
+++ Develop/seeds/product-tag-seeds.js
@@ -1,56 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 const { ProductTag } = require('../models');
 
-const productTagData = [
-  {
-    product_id: 1,
-    tag_id: 6,
-  },
-  {
-    product_id: 1,
-    tag_id: 7,
-  },
-  {
-    product_id: 1,
-    tag_id: 8,
-  },
-  {
-    product_id: 2,
-    tag_id: 6,
-  },
-  {
-    product_id: 3,
-    tag_id: 1,
-  },
-  {
-    product_id: 3,
-    tag_id: 3,
-  },
-  {
-    product_id: 3,
-    tag_id: 4,
-  },
-  {
-    product_id: 3,
-    tag_id: 5,
-  },
-  {
-    product_id: 4,
-    tag_id: 1,
-  },
-  {
-    product_id: 4,
-    tag_id: 2,
-  },
-  {
-    product_id: 4,
-    tag_id: 8,
-  },
-  {
-    product_id: 5,
-    tag_id: 3,
-  },
-];
+class ProductTag extends Model {}
 
-const seedProductTags = () => ProductTag.bulkCreate(productTagData);
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
 
-module.exports = seedProductTags;
+module.exports = ProductTag;
